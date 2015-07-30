b1526528201abefefff35879d57ea6bace535661
Jim Kang <jimkang@gmail.com>
2015-07-30 11:45:20 -0400

    Added autoHeaderHidePlugin and autoTitlePlugin. Updated to conform to other vendor plugins.

---
+++ vendor/plugins/edu.harvard.dce.paella.autoHeaderHidePlugin/autoHeaderHide.js
@@ -0,0 +1,30 @@
+Class ("paella.plugins.AutoHeaderHidePlugin", paella.EventDrivenPlugin, {
+  getName: function() {
+    return "edu.harvard.dce.paella.autoHeaderHidePlugin";
+  },
+
+  _pausedOnce: false,
+
+  getEvents: function(){
+    return [paella.events.play, paella.events.pause];
+  },
+
+  onEvent: function(event, params) {
+    thisClass = this;
+    if(event == 'paella:play'){
+      var duration = 1000;
+      if(thisClass._pausedOnce == false){
+        duration = 3000;
+      }
+      jQuery('#dceHeader').animate(
+          { top: '-60px' },
+          duration,
+          function() { thisClass._pausedOnce = true; }
+      );
+    } else if (event == 'paella:pause'){
+      jQuery('#dceHeader').animate({ top: '0px' }, 1000);
+    }
+  }
+});
+
+paella.plugins.autoHeaderHidePlugin = new paella.plugins.AutoHeaderHidePlugin();
---
+++ vendor/plugins/edu.harvard.dce.paella.autoTitlePlugin/autoTitlePlugin.js
@@ -0,0 +1,35 @@
+Class ("paella.plugins.AutoTitlePlugin", paella.EventDrivenPlugin, {
+  getName: function() {
+    return "edu.harvard.dce.paella.autoTitlePlugin";
+  },
+
+  getEvents: function(){
+    return [paella.events.loadPlugins];
+  },
+
+  _createSeriesTitleNode: function(){
+    var nodeToInsert = '';
+    try {
+      var dcObj = paella.matterhorn.serie['http://purl.org/dc/terms/'];
+      nodeToInsert = jQuery('<div class="seriesInfo" />').append(
+          jQuery('<a />').attr({
+            'title': 'Go to ' + dcObj.title[0].value,
+            'href': dcObj.description[0].value
+          }).html(
+            dcObj.title[0].value
+            ).append(
+              jQuery('<span class="seriesTitle" />').text(' :: ' + dcObj.subject[0].value)
+              )
+          );
+    } catch(err) {
+      console.log(err.message);
+    }
+    return nodeToInsert;
+  },
+
+  onEvent: function(event, params) {
+    jQuery('#dceHeader .primary').after(this._createSeriesTitleNode());
+  }
+});
+
+paella.plugins.autoTitlePlugin = new paella.plugins.AutoTitlePlugin();
