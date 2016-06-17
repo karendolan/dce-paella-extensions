75b8508541862973dcb3c23af11e51cefadc45ae
Karen Dolan <kdolan@dce.harvard.edu>
2016-06-17 09:48:52 -0400

    MATT-1999 top align option via url param

---
+++ vendor/plugins/edu.harvard.dce.paella.topAlignVideoPlugin/topAlignVideoPlugin.js
@@ -0,0 +1,42 @@
+// TopAlignVideoPlugin
+// purpose: Video takes all top space to provide non-overlapping room for the control bar.
+// MATT-1999 Top aligned video required to embed player compactly in iframe without obscursing video with control bar.
+// Impl Strategy: top align video container and video elemements to overwrite default core calculations.
+Class ("paella.plugins.TopAlignVideoPlugin", paella.EventDrivenPlugin, {
+  
+  getName: function () {
+    return "edu.harvard.dce.paella.topAlignVideoPlugin";
+  },
+  
+  getEvents: function () {
+    return[paella.events.setProfile, paella.events.singleVideoReady, paella.events.resize, paella.events.loadStarted];
+  },
+  
+  checkEnabled: function (onSuccess) {
+    // expecting ?align=top in url
+    var topAlign = paella.utils.parameters.get('align');
+    onSuccess((topAlign == 'top'));
+  },
+  
+  onEvent: function (eventType, params) {
+    base.log.debug("Top align recieved event " + eventType);
+    if (eventType == 'paella:singleVideoReady') {
+      setTimeout(this._topAlign(), 10000);
+    } else {
+     this._topAlign(); 
+    }
+  },
+  
+  _topAlign: function(){
+    // MATT-1999 Align video container and elements to top
+    paella.player.videoContainer.container.domElement.style.top = "0%";
+    var elementChildren = paella.player.videoContainer.container.domElement.children;
+    for (var i = 0; i < elementChildren.length; i++) {
+      base.log.debug("Top aligning " + elementChildren[i].id + ", " + elementChildren[i].style.top);
+      elementChildren[i].style.top = "0%";
+    }
+  }
+  
+});
+
+paella.plugins.topAlignVideoPlugin = new paella.plugins.TopAlignVideoPlugin();
